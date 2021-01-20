d72d3b6f0dc6fd6227c02a620c52087261190f76
aaksi <yyff91@mail.ru>
2021-01-20 22:04:24 +0300

    added page item-price, one-product, page, price-page, products

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,7 @@
 $(function(){
     $('.questions__accordion-title').click(function(){
         const parent = $(this).parent();
-
+        
         if(parent.hasClass('questions__accordion-item--active')){
             parent.removeClass('questions__accordion-item--active');
         }else{
@@ -10,15 +10,14 @@ $(function(){
             parent.addClass('questions__accordion-item--active')
         }
         
-        // $(".questions__accordion-descr").show(400);   
+
+    });
+
+    $('.blog__slider').slick({
+        dots: true,
+        prevArrow: false,
+        nextArrow: false,
     });
     
-
-
-   
-
+    
 });
-
-
-
-// $(this).parent('.questions__accordion-item').toggleClass('questions__accordion-item--active');
