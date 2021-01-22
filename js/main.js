0cc09db65f48bff703a67108c1e7736217634159
aaksi <73750375+aaksi@users.noreply.github.com>
2021-01-22 20:47:45 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+
+$(function(){
+    $('.questions__accordion-title').click(function(){
+        const parent = $(this).parent();
+        
+        if(parent.hasClass('questions__accordion-item--active')){
+            parent.removeClass('questions__accordion-item--active');
+        }else{
+            $('.questions__accordion-item').removeClass('questions__accordion-item--active')
+            parent.addClass('questions__accordion-item--active')
+        }
+        
+
+    });
+
+    $('.blog__slider').slick({
+        dots: true,
+        prevArrow: false,
+        nextArrow: false,
+    });
+    $('.company__slider').slick({
+        dots: true,
+        prevArrow: false,
+        nextArrow: false,
+    });
+
+    $('.menu__btn').on("click", function(){
+        $(this).toggleClass('menu__btn--active');
+        $('.menu__list').toggleClass('menu__list--active');
+        $('.menu__back').toggleClass('menu__back--active');
+    });
+    
+    
+});
